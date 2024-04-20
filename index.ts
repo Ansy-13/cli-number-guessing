33c7a10f0c1a17ac78add89896f3d48466087a62
Ansy-13 <aimahadia13@gmail.com>
2024-04-20 13:35:37 +0500

    a

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 6+1 );
+
+
+
+const answer=await inquirer.prompt([
+    {
+        name:"userguessedNumber",
+        type:"number",
+        message:"please guess a number between 1-6:",
+    },
+]);
+
+if (answer.userguessedNumber=== randomNumber){
+    console.log("congratulation you gussed the right number")
+}else{
+    console.log("you gussed the wrong number")
+}
+
